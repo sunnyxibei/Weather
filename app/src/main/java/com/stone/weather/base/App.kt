acc19f28cc4a5fcbee9fe3759aa6e7dd9498573e
sunnyxibei <sunnyxibei@foxmail.com>
2017-11-09 08:52:45 +0800

    [update] 实现单例

---
+++ app/src/main/java/com/stone/weather/base/App.kt
@@ -0,0 +1,21 @@
+package com.stone.weather.base
+
+import android.app.Application
+import kotlin.properties.Delegates
+
+/**
+ * Created by sunnyxibei on 17-11-8.
+ */
+class App : Application() {
+
+
+    // define application instance
+    companion object {
+        var instance: App by Delegates.notNull()
+    }
+
+    override fun onCreate() {
+        super.onCreate()
+        instance = this
+    }
+}
